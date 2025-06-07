83a4efe5652dd27032e910261ece1bb09bf2e930
Gustav0TM <gustavoatmunguia@gmail.com>
2025-06-07 03:15:41 -0500

    Agregar endpoint /datos compatible con Android

--- models/agent_data.py
+++ models/agent_data.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 latest_agent_data = {}
 
@@ -6,10 +7,17 @@ def save_agent_data(data):
     hostname = data.get("hostname")
     if hostname:
         data["timestamp"] = int(datetime.datetime.now().timestamp())
-        
-        
-
+                
         latest_agent_data[hostname] = data
+
+def limpiar_dispositivos_inactivos():
+    ahora = time.time()
+    limite = ahora - 300  # 5 minutos = 300 segundos
+    # Filtra y deja solo los dispositivos con timestamp reciente
+    activos = {k: v for k, v in latest_agent_data.items() if v.get("timestamp", 0) > limite}
+    # Actualiza el dict global
+    latest_agent_data.clear()
+    latest_agent_data.update(activos)
 
 def get_all_agent_data():
     return latest_agent_data
